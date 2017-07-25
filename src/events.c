f96d86eed88a1de3b51f61eb8f0aded1950d8953
Dil3mm4 <francoismanzo@gmail.com>
2017-07-25 04:41:50 +0200

    Trying implement of autofire

--- src/events.c
+++ src/events.c
@@ -193,7 +193,7 @@ __myevic__ void GetUserInput()
 
 	if ( ( !PE0 || AutoPuffTimer ) && PD2 && PD3 )
 	{
-		if ( !PE0 ) AutoPuffTimer = 0;
+		if ( !PE0 && !gFlags.autopuff) AutoPuffTimer = 0;
 
 		if (( LastInputs == 5 ) || ( LastInputs == 6 ))
 			return;
@@ -202,11 +202,34 @@ __myevic__ void GetUserInput()
 	}
 	else
 	{
+		
+		/*trying to implement autopuff*/
+		if (gFlags.autopuff)
+ 			{
+ 				if ( LastInputs == 1 )
+ 					{
+ 						AutoPuffTimer = 0;
+ 						gFlags.autopuff = 0;
+ 						StopFire();
+ 					}
+ 			}
+		/*eof autopuff*/
+		
 		if ( gFlags.firing )
 		{
 			if ( LastInputs == 1 )
 			{
+				
 				StopFire();
+				/*trying to implement autopuff*/
+				if (gFlags.autopuff)
+{
+AutoPuffTimer = 0;
+gFlags.autopuff = 0;
+StopFire();
+}
+				/*eof*/
+				
 			}
 			gFlags.user_idle = 1;
 			LastInputs = -1;
